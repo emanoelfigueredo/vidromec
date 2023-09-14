b8f3fb22b84208c3efcff8ba2dbe976fbf9f26bc
efigueredo <emanoel.figueredo.1@gmail.com>
2023-09-14 18:15:48 -0300

    modulo de home criado.

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
